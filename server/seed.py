658dbea94a6595c0baa33dca98ca184ba5190b9b
rl4694 <rl4694@nyu.edu>
2025-10-17 16:14:56 -0400

    Add nations seeding

--- server/seed.py
+++ server/seed.py
@@ -6,32 +6,26 @@ APIs. Be sure to empty the database before running this file.
 import requests
 import time
 from server import cities as ct
+from server import nations as nt
 
 
 RAPID_API_HOST = 'wft-geo-db.p.rapidapi.com'
 RAPID_API_KEY = 'ba62340b10msh533fb3cfda50da1p1eff9djsn8ca40e19327b'
 CITIES_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities'
+NATIONS_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/countries'
 RESULTS_PER_PAGE = 10
 COOLDOWN_SEC = 2
 
 
-def fetch_cities(offset):
+def fetch_rapid_api(url, params):
     """
-    Return JSON city data from GeoDB Cities API
+    Return JSON data from GeoDB API
     """
-    if offset < 0:
-        raise ValueError(f'Offset cannot be negative {offset=}')
-
     headers = {
         'x-rapidapi-host': RAPID_API_HOST,
         'x-rapidapi-key': RAPID_API_KEY,
     }
-    params = {
-        'sort': '-population',
-        'limit': RESULTS_PER_PAGE,
-        'offset': offset,
-    }
-    res = requests.get(CITIES_URL, headers=headers, params=params)
+    res = requests.get(url, headers=headers, params=params)
     if res.status_code != 200:
         raise ConnectionError(
             f'Could not retrieve GeoDB cities: {res.status_code=}'
@@ -51,7 +45,11 @@ def seed_cities(num_cities):
     offset = 0
     for i in range(num_cities // RESULTS_PER_PAGE):
         # Fetch city data
-        res = fetch_cities(offset)
+        res = fetch_rapid_api(CITIES_URL, {
+            'sort': '-population',
+            'limit': RESULTS_PER_PAGE,
+            'offset': offset,
+        })
         if 'data' not in res:
             raise ValueError(f'No data found in GeoDB cities response: {res=}')
 
@@ -62,6 +60,43 @@ def seed_cities(num_cities):
                 ct.STATE: city.get('region', ''),
                 ct.NATION: city.get('country', ''),
             })
+        
+        # Print status
+        print(f"Seeding cities: {ct.length() * 100 // num_cities}%")
+
+        # Wait for rate-limit to wear off
+        time.sleep(COOLDOWN_SEC)
+        offset += RESULTS_PER_PAGE
+
+
+def seed_nations():
+    """
+    Add all nation data to our database
+    """
+    offset = 0
+    num_nations = None
+    while num_nations is None or offset <= num_nations:
+        # Fetch city data
+        res = fetch_rapid_api(NATIONS_URL, {
+            'limit': RESULTS_PER_PAGE,
+            'offset': offset,
+        })
+        if 'data' not in res or 'metadata' not in res:
+            raise ValueError(f'No data found in GeoDB nations response: {res=}')
+        
+        # Stop if we fetched all countries
+        if len(res['data']) == 0:
+            break
+
+        # Add city data to database
+        for country in res['data']:
+            nt.create({
+                nt.NAME: country.get('name', ''),
+            })
+
+        # Print status
+        num_nations = res['metadata']['totalCount']
+        print(f"Seeding countries: {nt.length() * 100 // num_nations}%")
 
         # Wait for rate-limit to wear off
         time.sleep(COOLDOWN_SEC)
@@ -69,4 +104,5 @@ def seed_cities(num_cities):
 
 
 if __name__ == '__main__':
-    seed_cities(20)
+    seed_cities(100)
+    seed_nations()
--- server/tests/test_seed.py
+++ server/tests/test_seed.py
@@ -2,19 +2,24 @@ import pytest
 import time
 from unittest.mock import patch
 from server import cities as ct
+from server import nations as nt
 from server import seed as sd
 
 
-class TestFetchCities:
+class TestFetchRapidApi:
     @patch('server.seed.requests.get', autospec=True)
-    def test_error_response(self, mock_get):
+    def test_valid(self, mock_get):
+        data = { 'hello': 'world' }
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = data
+        res = sd.fetch_rapid_api(sd.CITIES_URL, {})
+        assert res == data
+
+    @patch('server.seed.requests.get', autospec=True)
+    def test_error_code(self, mock_get):
         mock_get.return_value.status_code = 400
         with pytest.raises(ConnectionError):
-            sd.fetch_cities(0)
-
-    def test_negative_offset(self):
-        with pytest.raises(ValueError):
-            sd.fetch_cities(-1)
+            sd.fetch_rapid_api(sd.CITIES_URL, {})
 
 
 class TestSeedCities:
@@ -33,7 +38,44 @@ class TestSeedCities:
         old_count = ct.length()
         sd.seed_cities(sd.RESULTS_PER_PAGE)
         assert ct.length() > old_count
+    
+    @patch('time.sleep')
+    @patch('server.seed.requests.get', autospec=True)
+    def test_missing_data(self, mock_get, mock_sleep):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {}
+
+        with pytest.raises(ValueError):
+            sd.seed_cities(sd.RESULTS_PER_PAGE)
 
     def test_negative_cities(self):
         with pytest.raises(ValueError):
             sd.seed_cities(-1)
+
+
+class TestSeedNations:
+    @patch('time.sleep')
+    @patch('server.seed.requests.get', autospec=True)
+    def test_valid(self, mock_get, mock_sleep):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            'data': [{
+                'name': 'my nation',
+            }],
+            'metadata': {
+                'totalCount': 1,
+            },
+        }
+
+        old_count = nt.length()
+        sd.seed_nations()
+        assert nt.length() > old_count
+    
+    @patch('time.sleep')
+    @patch('server.seed.requests.get', autospec=True)
+    def test_missing_data(self, mock_get, mock_sleep):
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {}
+
+        with pytest.raises(ValueError):
+            sd.seed_nations()
