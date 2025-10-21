e65b5bf5c271c850b53e9e569d5b3808ec6e7534
sjp9 <sjp9482@nyu.edu>
2025-10-21 13:07:58 -0400

    Add CRUD API endpoints for cities

--- server/cities.py
+++ server/cities.py
@@ -1,10 +1,22 @@
+from flask import Flask, request
+from flask_restx import Resource, Api, fields # Namespace
+
 MIN_ID_LEN = 1
 NAME = 'name'
 STATE = 'state'
 NATION = 'nation'
 
+CITIES_EP = '/cities'
+CITIES_RESP = 'cities'
+
 cities = {}
 
+city_model = api.model(
+    'City',
+    {
+        'name': fields.String(required=True, description='City Name')
+    }
+)
 
 def is_valid_id(_id: str) -> bool:
     """Return True if _id is a non-empty string."""
@@ -47,3 +59,47 @@ def create(fields: dict) -> str:
 def read() -> dict:
     """Return all cities stored."""
     return cities
+
+
+# CITIES ENDPOINTS
+@api.route(CITIES_EP)
+class CityList(Resource):
+    @api.doc('list_cities')
+    def get(self):
+        cities = ct.read()
+        return {
+            CITIES_RESP: cities,
+        }
+
+    @api.expect(city_model)
+    @api.doc('create_city')
+    def post(self):
+        data = request.json
+        city_id = ct.create(data)
+        return {'id': city_id, **data}, 201
+
+
+@api.route('/cities/<string:city_id>')
+class City(Resource):
+    @api.doc('get_city')
+    def get(self, city_id):
+        city = ct.cities.get(city_id)
+        if not city:
+            api.abort(404, "City not found")
+        return {'id': city_id, **city}
+
+    @api.expect(city_model)
+    @api.doc('update_city')
+    def put(self, city_id):
+        if city_id not in ct.cities:
+            api.abort(404, "City not found")
+        data = request.json
+        ct.cities[city_id] = data
+        return {'id': city_id, **data}
+
+    @api.doc('delete_city')
+    def delete(self, city_id):
+        if city_id not in ct.cities:
+            api.abort(404, "City not found")
+        del ct.cities[city_id]
+        return '', 204
