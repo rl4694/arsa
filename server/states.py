67f1d1672d4fb06d623a6da9944d6dda482b9587
sjp9 <sjp9482@nyu.edu>
2025-10-21 18:02:03 -0400

    Modularize CRUD API endpoints for states

--- server/states.py
+++ server/states.py
@@ -1,7 +1,10 @@
 # server/states.py
+from flask import request
+from flask_restx import Resource, Namespace, fields
 
 MIN_ID_LEN = 1
 NAME = 'name'
+NATION = 'nation'
 
 # In-memory storage for states
 states = {}
@@ -33,5 +36,77 @@ def create(fields: dict) -> str:
         raise ValueError(f'Name missing in fields: {fields.get(NAME)}')
 
     _id = str(len(states) + 1)
-    states[_id] = fields
+    states[_id] = {
+        NAME: fields.get(NAME),
+        NATION: fields.get(NATION)
+    }
     return _id
+
+
+def read() -> dict:
+    return states
+
+
+def update(state_id: str, data: dict):
+    if state_id not in states:
+        raise KeyError("State not found")
+    states[state_id] = {
+        NAME: data.get(NAME),
+        NATION: data.get(NATION)
+    }
+
+
+def delete(state_id: str):
+    if state_id not in states:
+        raise KeyError("State not found")
+    del states[state_id]
+
+
+api = Namespace('states', description='States CRUD operation')
+
+state_model = api.model('State', {
+    'name': fields.String(required=True, description='State Name'),
+    'nation': fields.String(description='Nation Name')
+})
+
+
+# STATES ENDPOINTS
+@api.route('/')
+class StateList(Resource):
+    @api.doc('list_states')
+    def get(self):
+        return {'states': read()]
+
+    @api.expect(state_model)
+    @api.doc('create_state')
+    def post(self):
+        data = request.json
+        state_id = create(data)
+        return {'id': state_id, **data}, 201
+
+
+@api.route('/<string:state_id>')
+class State(Resource):
+    @api.doc('get_state')
+    def get(self, state_id):
+        state = states.get(state_id)
+        if not state:
+            api.abort(404, "State not found")
+        return {'id': state_id, **state}
+
+    @api.expect(state_model)
+    @api.doc('update_state')
+    def put(self, state_id):
+        try:
+            update(state_id, request.json)
+            return {'id': state_id, **states[state_id]}
+        except KeyError:
+            api.abort(404, "State not found")
+
+    @api.doc('delete_state')
+    def delete(self, state_id):
+        try:
+            delete(state_id)
+            return '', 204
+        except KeyError:
+            api.abort(404, "State not found")
