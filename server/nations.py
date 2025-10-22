f94c3651c3dd67016e7ba2c80ca1f0419f810c83
sjp9 <sjp9482@nyu.edu>
2025-10-21 21:40:18 -0400

    Modularize CRUD API endpoints for nations

--- server/nations.py
+++ server/nations.py
@@ -1,4 +1,6 @@
 # server/nations.py
+from flask import request
+from flask_restx import Resource, Namespace, fields
 
 MIN_ID_LEN = 1
 NAME = 'name'
@@ -26,9 +28,74 @@ def create(fields: dict) -> str:
 
     # Pre-pending underscore because id is a built-in Python function
     _id = str(len(nations) + 1)
-    nations[_id] = fields
+    nations[_id] = {
+        NAME: fields.get(NAME)
+    }
     return _id
 
 
 def read() -> dict:
     return nations
+
+
+def update(nation_id: str, data: dict):
+    if nation_id not in nations:
+        raise KeyError("Nation not found")
+    nations[nation_id] = {
+        NAME: data.get(NAME)
+    }
+
+
+def delete(nation_id: str):
+    if nation_id not in nations:
+        raise KeyError("Nation not found")
+    del nations[nation_id]
+
+
+api = Namespace('nations', description='Nations CRUD operations')
+
+nation_model = api.model('Nation', {
+    'name': fields.String(required=True, description='Nation Name')
+})
+
+
+# NATIONS ENDPOINTS
+@api.route('/')
+class NationList(Resource):
+    @api.doc('list_nations')
+    def get(self):
+        return {'nations': read()}
+
+    @api.expect(nation_model)
+    @api.doc('create_nation')
+    def post(self):
+        data = request.json
+        nation_id = create(data)
+        return {'id': nation_id, **data}, 201
+
+
+@api.route('/<string:nation_id>')
+class Nation(Resource):
+    @api.doc('get_nation')
+    def get(self, nation_id):
+        nation = nations.get(nation_id)
+        if not nation:
+            api.abort(404, "Nation not found")
+        return {'id': nation_id, **nation}
+
+    @api.expect(nation_model)
+    @api.doc('update_nation')
+    def put(self, nation_id):
+        try:
+            update(nation_id, request.json)
+            return {'id': nation_id, **nations[nation_id]}
+        except KeyError:
+            api.abort(404, "Nation not found")
+
+    @api.doc('delete_nation')
+    def delete(self, nation_id):
+        try:
+            delete(nation_id)
+            return '', 204
+        except KeyError:
+            api.abort(404, "Nation not found")
