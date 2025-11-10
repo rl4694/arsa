14cfe20b6dff0580c0d86cd7b7dbc71d48c5b591
sjp9 <sjp9482@nyu.edu>
2025-11-10 18:05:40 -0500

    Enhance db_connect.py: add decorator to all DB functions and improve documentation
    - Applied @needs_db decorator to all database functions (create, read, read_one, delete, update, read_dict, fetch_all_as_dict)
    - Decorator automatically establishes DB connection before each function call
    - Added comprehensive docstrings with Args and Returns documentation
    - Improved code quality and consistency
    - Ensures all MongoDB interactions go through db_connect.py as required

--- data/db_connect.py
+++ data/db_connect.py
@@ -4,22 +4,27 @@ We may be required to use a new database at any point.
 """
 import os
 from functools import wraps
-
 import pymongo as pm
 
 LOCAL = "0"
 CLOUD = "1"
-
 SE_DB = 'seDB'
 
 client = None
-
 MONGO_ID = '_id'
 
 
-def needs_db(func, *args, **kwargs):
+def needs_db(func):
     """
-    This provides a decorator interface for connect_db
+    Decorator that ensures a database connection exists before executing
+    a function. Automatically calls connect_db() if the client is not
+    already connected.
+    
+    Usage:
+        @needs_db
+        def my_function():
+            # Access global client here
+            pass
     """
     @wraps(func)
     def wrapper(*args, **kwargs):
@@ -33,10 +38,15 @@ def needs_db(func, *args, **kwargs):
 def connect_db():
     """
     This provides a uniform way to connect to the DB across all uses.
-    Returns a mongo client object... maybe we shouldn't?
-    Also set global client variable.
-    We should probably either return a client OR set a
-    client global.
+    Returns a mongo client object and also set global client variable.
+    
+    Connection priority:
+    1. Check CLOUD_MONGO environment variable
+    2. If CLOUD is set, use cloud MongoDB with credentials
+    3. Otherwise, use local MongoDB instance
+    
+    Raises:
+        ValueError: If cloud MongoDB is requested but password not set
     """
     global client
     if client is None:  # not connected yet!
@@ -45,7 +55,7 @@ def connect_db():
             password = os.environ.get('MONGO_PASSWD')
             if not password:
                 raise ValueError('You must set your password '
-                                 + 'to use Mongo in the cloud.')
+                                + 'to use Mongo in the cloud.')
             print('Connecting to Mongo in the cloud.')
             client = pm.MongoClient(f'mongodb+srv://gcallah:{password}'
                                     + '@koukoumongo1.yud9b.mongodb.net/'
@@ -57,45 +67,97 @@ def connect_db():
 
 
 def convert_mongo_id(doc: dict):
+    """
+    Convert MongoDB ObjectId to string for JSON serialization.
+    """
     if MONGO_ID in doc:
-        # Convert mongo ID to a string so it works as JSON
         doc[MONGO_ID] = str(doc[MONGO_ID])
 
 
+@needs_db
 def create(collection, doc, db=SE_DB):
     """
     Insert a single doc into collection.
+    
+    Args:
+        collection: Collection name
+        doc: Document to insert
+        db: Database name (default: SE_DB)
+    
+    Returns:
+        InsertResult with inserted_id
     """
     print(f'{db=}')
     return client[db][collection].insert_one(doc)
 
 
+@needs_db
 def read_one(collection, filt, db=SE_DB):
     """
-    Find with a filter and return on the first doc found.
-    Return None if not found.
+    Find with a filter and return the first doc found.
+    Converts MongoDB ID to string.
+    
+    Args:
+        collection: Collection name
+        filt: Filter dictionary
+        db: Database name (default: SE_DB)
+    
+    Returns:
+        First matching document or None if not found
     """
     for doc in client[db][collection].find(filt):
         convert_mongo_id(doc)
         return doc
+    return None
 
 
+@needs_db
 def delete(collection: str, filt: dict, db=SE_DB):
     """
-    Find with a filter and return on the first doc found.
+    Delete document(s) matching filter from collection.
+    
+    Args:
+        collection: Collection name
+        filt: Filter dictionary
+        db: Database name (default: SE_DB)
+    
+    Returns:
+        Number of documents deleted
     """
     print(f'{filt=}')
     del_result = client[db][collection].delete_one(filt)
     return del_result.deleted_count
 
 
+@needs_db
 def update(collection, filters, update_dict, db=SE_DB):
+    """
+    Update document(s) in collection matching filters.
+    
+    Args:
+        collection: Collection name
+        filters: Filter dictionary to match documents
+        update_dict: Dictionary of fields to update
+        db: Database name (default: SE_DB)
+    
+    Returns:
+        UpdateResult
+    """
     return client[db][collection].update_one(filters, {'$set': update_dict})
 
 
+@needs_db
 def read(collection, db=SE_DB, no_id=True) -> list:
     """
-    Returns a list from the db.
+    Returns a list of all documents from the collection.
+    
+    Args:
+        collection: Collection name
+        db: Database name (default: SE_DB)
+        no_id: If True, removes _id field from documents (default: True)
+    
+    Returns:
+        List of documents
     """
     ret = []
     for doc in client[db][collection].find():
@@ -107,7 +169,20 @@ def read(collection, db=SE_DB, no_id=True) -> list:
     return ret
 
 
+@needs_db
 def read_dict(collection, key, db=SE_DB, no_id=True) -> dict:
+    """
+    Returns documents as a dictionary keyed by specified field.
+    
+    Args:
+        collection: Collection name
+        key: Field to use as dictionary key
+        db: Database name (default: SE_DB)
+        no_id: If True, removes _id field from documents (default: True)
+    
+    Returns:
+        Dictionary with specified key as keys and documents as values
+    """
     recs = read(collection, db=db, no_id=no_id)
     recs_as_dict = {}
     for rec in recs:
@@ -115,7 +190,19 @@ def read_dict(collection, key, db=SE_DB, no_id=True) -> dict:
     return recs_as_dict
 
 
+@needs_db
 def fetch_all_as_dict(key, collection, db=SE_DB):
+    """
+    Fetch all documents and return as dictionary keyed by specified field.
+    
+    Args:
+        key: Field to use as dictionary key
+        collection: Collection name
+        db: Database name (default: SE_DB)
+    
+    Returns:
+        Dictionary with specified key as keys and documents as values
+    """
     ret = {}
     for doc in client[db][collection].find():
         del doc[MONGO_ID]
