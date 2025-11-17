3266c9a7d7664dd4f09d02e7549f4c2a56671c34
sjp9 <sjp9482@nyu.edu>
2025-11-17 18:53:43 -0500

    Update test_nations.py to work with MongoDB calls

--- server/controllers/tests/test_nations.py
+++ server/controllers/tests/test_nations.py
@@ -2,6 +2,12 @@ import pytest
 import server.controllers.nations as nt
 import server.common as common
 
+SAMPLE_NAME = 'test_nation'
+SAMPLE_KEY = SAMPLE_NAME
+SAMPLE_NATION = {
+    nt.NAME: SAMPLE_NAME,
+}
+
 
 @pytest.fixture(autouse=True)
 def _reset_store():
@@ -13,12 +19,21 @@ def _reset_store():
         nt.nations.clear()
 
 
+class TestLength:
+    def test_basic(self):
+        old_length = nt.length()
+        _id = nt.create(SAMPLE_NATION)
+        assert nt.length() == old_length + 1
+        nt.delete(_id)
+        assert nt.length() == old_length
+
+
 class TestCreate:
     def test_valid(self):
-        old_length = nt.length()
-        _id = nt.create({nt.NAME: "test1"})
+        _id = nt.create(SAMPLE_NATION)
         assert common.is_valid_id(_id)
-        assert nt.length() > old_length
+        nations = nt.read()
+        assert SAMPLE_KEY in nations
         nt.delete(_id)
 
     def test_non_dict(self):
@@ -32,8 +47,28 @@ class TestCreate:
 
 class TestRead:
     def test_basic(self):
-        _id = nt.create({nt.NAME: "test1"})
+        _id = nt.create(SAMPLE_NATION)
         nations = nt.read()
         assert isinstance(nations, dict)
+        assert SAMPLE_KEY in nations
         assert len(nations) > 0
         nt.delete(_id)
+
+
+class TestUpdate:
+    def test_basic(self):
+        _id = nt.create(SAMPLE_NATION)
+        new_name = 'new_nation'
+        nt.update(_id, {nt.NAME: new_name})
+        nations = nt.read()
+        assert new_name in nations
+        assert nations[new_name][nt.NAME] == new_name
+        nt.delete(_id)
+
+
+class TestDelete:
+    def test_basic(self):
+        _id = nt.create(SAMPLE_NATION)
+        nt.delete(_id)
+        nations = nt.read()
+        assert SAMPLE_KEY not in nations
