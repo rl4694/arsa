00487b7a3e209935be7cdc156b580cf38a76264c
ryljin <ryanjin102354@gmail.com>
2025-12-02 19:57:15 -0800

    Updated nations with new nation code id based system

--- server/controllers/nations.py
+++ server/controllers/nations.py
@@ -5,16 +5,19 @@ This file implements CRUD operations for nations.
 from flask import request
 from flask_restx import Resource, Namespace, fields
 from server.controllers.crud import CRUD
+import pycountry
 
 NATIONS_RESP = 'nations'
 COLLECTION = 'nations'
 NAME = 'name'
-KEY = (NAME,)
+CODE = 'code'
+KEY = (CODE,)
 
 nations = CRUD(
     COLLECTION,
     KEY,
     {
+        CODE: str,
         NAME: str,
     }
 )
@@ -39,6 +42,16 @@ class NationList(Resource):
     @api.doc('create_nation')
     def post(self):
         data = request.json
+
+        try:
+            country = pycountry.countries.get(name=data['name'])
+            code = country.alpha_2
+        except:
+            api.abort(400, f"Invalid nation name: {data['name']}")
+
+        data['code'] = code
+        data['_id'] = code
+
         _id = nations.create(data, recursive=False)
         created = nations.select(_id)
         return {NATIONS_RESP: created}, 201
@@ -58,7 +71,19 @@ class Nation(Resource):
     @api.doc('update_nation')
     def put(self, nation_id):
         try:
-            nations.update(nation_id, request.json)
+            payload = request.json
+            if 'name' in payload:
+                try:
+                    country = pycountry.countries.get(name=payload['name'])
+                    payload['code'] = country.alpha_2
+                except:
+                    api.abort(400, f"Invalid nation name: {payload['name']}")
+                nations.delete(nation_id)
+                new_id = nations.create(payload, recursive=False)
+                record = nations.select(new_id)
+                return {NATIONS_RESP: record}
+
+            nations.update(nation_id, payload)
             record = nations.select(nation_id)
             return {NATIONS_RESP: record}
         except KeyError:
