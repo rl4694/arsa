a56763f42dc37a328c5d38625e6e3d8d64ba89ae
Erik Lee <el3844@nyu.edu>
2026-02-24 14:36:49 -0500

    Added tests for various dates

--- server/controllers/tests/test_natural_disasters.py
+++ server/controllers/tests/test_natural_disasters.py
@@ -29,12 +29,64 @@ class TestValidate:
             nd.DESCRIPTION: SAMPLE_DESCRIPTION,
         })
 
+    def test_valid_3_digit_year(self):
+        disasters.validate({
+            nd.NAME: SAMPLE_NAME,
+            nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
+            nd.DATE: '500-01-01',
+            nd.LATITUDE: 1.0,
+            nd.LONGITUDE: 1.0,
+            nd.DESCRIPTION: SAMPLE_DESCRIPTION,
+        })
+
+    def test_valid_negative_year(self):
+        disasters.validate({
+            nd.NAME: SAMPLE_NAME,
+            nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
+            nd.DATE: '-500-01-01',
+            nd.LATITUDE: 1.0,
+            nd.LONGITUDE: 1.0,
+            nd.DESCRIPTION: SAMPLE_DESCRIPTION,
+        })
+
+    def test_valid_1_digit_year(self):
+        disasters.validate({
+            nd.NAME: SAMPLE_NAME,
+            nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
+            nd.DATE: '5-01-01',
+            nd.LATITUDE: 1.0,
+            nd.LONGITUDE: 1.0,
+            nd.DESCRIPTION: SAMPLE_DESCRIPTION,
+        })
+
     def test_invalid_date(self):
         with pytest.raises(ValueError):
             disasters.validate({
                 nd.NAME: SAMPLE_NAME,
                 nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
                 nd.DATE: 'invalid-date',
+                nd.LATITUDE: 1.0,
+                nd.LONGITUDE: 1.0,
+                nd.DESCRIPTION: SAMPLE_DESCRIPTION,
+            })
+
+    def test_invalid_month(self):
+        with pytest.raises(ValueError):
+            disasters.validate({
+                nd.NAME: SAMPLE_NAME,
+                nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
+                nd.DATE: '2000-13-01',
+                nd.LATITUDE: 1.0,
+                nd.LONGITUDE: 1.0,
+                nd.DESCRIPTION: SAMPLE_DESCRIPTION,
+            })
+
+    def test_invalid_day(self):
+        with pytest.raises(ValueError):
+            disasters.validate({
+                nd.NAME: SAMPLE_NAME,
+                nd.DISASTER_TYPE: SAMPLE_DISASTER_TYPE,
+                nd.DATE: '2000-02-30',
                 nd.LATITUDE: 1.0,
                 nd.LONGITUDE: 1.0,
                 nd.DESCRIPTION: SAMPLE_DESCRIPTION,
